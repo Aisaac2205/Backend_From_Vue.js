22a560f053960df0e092f4ff6d10c75f55740e00
Isaac Flores <isaacflores2205@gmail.com>
2025-09-25 22:28:10 -0600

    fix: Configurar servido correcto de assets con MIME types apropiados y rutas catch-all mejoradas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return file_get_contents(public_path('index.html'));
 });
+
+// Servir assets con el tipo MIME correcto
+Route::get('/assets/{file}', function ($file) {
+    $path = public_path('assets/' . $file);
+    
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    
+    $extension = pathinfo($path, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'js' => 'application/javascript',
+        'css' => 'text/css',
+        'map' => 'application/json'
+    ];
+    
+    $mimeType = $mimeTypes[$extension] ?? 'text/plain';
+    
+    return response()->file($path, [
+        'Content-Type' => $mimeType
+    ]);
+})->where('file', '.*');
+
+// Servir archivos estáticos
+Route::get('/favicon.ico', function () {
+    return response()->file(public_path('favicon.ico'));
+});
+
+Route::get('/vite.svg', function () {
+    return response()->file(public_path('vite.svg'));
+});
+
+Route::get('/robots.txt', function () {
+    return response()->file(public_path('robots.txt'));
+});
+
+// Ruta catch-all para Vue Router (debe estar al final)
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
